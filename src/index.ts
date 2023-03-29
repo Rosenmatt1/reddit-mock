032c98ef0709e2468d4a4ace096fc2068d79105f
Matthew Rosenberg <rosenmatt1@gmail.com>
2023-03-28 21:04:12 -0600

    Created dta for a table in posts

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,19 @@
-import { } from "@mikro-orm/core"
+import {  MikroORM } from "@mikro-orm/core"
+import { __prod__ } from "./constants"
+import { Post } from "./entities/Post"
 
-const orm = MikroORM.init()
+const main = async () => {
+  const orm =  await MikroORM.init({
+    entities: [Post],
+    dbName: 'reddit',
+    type: 'postgresql',
+    debug: !__prod__
+  })
 
-console.log("hello world")
+  const post = orm.em.create(Post, {title: 'my first post'})
+  await orm.em.persistAndFlush(post)
+  // await orm.em.nativeInsert(Post, {title: 'my first post'}
+}
+
+
+main()
